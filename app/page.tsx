2b214598e53773b3314df33d30dcdd00852f62a8
shibifsp <faseehfsp000@gmail.com>
2025-03-15 15:08:39 +0530

    testing responsive

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <>
       <Header />
       <main className="h-[calc(100vh-84px)] flex items-center justify-center">
-        <h1 className=" text-4xl  font-bold  mb-10">
+        <h1 className=" text-4xl text-center font-bold mb-10 px-4">
           Practices from Dribbble
         </h1>
       </main>
